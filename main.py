52d926821570993aa888d83d55f5cf116c739eae
Your Name <you@example.com>
2025-12-25 22:55:07 +0800

    fix: 修正部署啟動進入點以支援 Zeabur

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from station!")
-
+import uvicorn
+import os
 
 if __name__ == "__main__":
-    main()
+    # 讀取 Zeabur 自動分配的 PORT，若無則預設 8080
+    port = int(os.getenv("PORT", 8080))
+    # 啟動 app 資料夾下的 main.py 裡的 app
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port)
